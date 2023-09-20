6df6da33c6f1b4c170c7176bc60d46e6b8d1d5dc
Antonio Filho <antoniodollmichael@gmail.com>
2023-09-20 00:44:46 -0300

    change all

---
+++ ProjetoLivrariaAPI/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoLivrariaAPI.Models;
+
+namespace ProjetoLivrariaAPI.Data {
+
+    //A classe DataContext é a classe responsável em criar nossa conexão e comunicação com o banco de dados. 
+    public class DataContext : DbContext {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Rental> Rentals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity<Rental>()
+                   .HasKey(R => new { R.UserId, R.BookId });
+            builder.Entity<Book>()
+                .HasKey(B => new { B.PublisherId });
+        }
+    }
+}
