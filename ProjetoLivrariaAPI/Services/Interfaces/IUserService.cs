8daef7b10bee9ddafdb31152a2251bbfb98ad8a6
Antonio Filho <antoniodollmichael@gmail.com>
2023-10-04 01:56:50 -0300

    por hoje não consigo mais

---
+++ ProjetoLivrariaAPI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using ProjetoLivrariaAPI.Dtos.User;
+
+namespace ProjetoLivrariaAPI.Services.Interfaces {
+    public interface IUserService {
+
+        Task<ResultService<UserDto>> CreateAsync(UserDto userDto);
+    }
+}
---
+++ ProjetoLivrariaAPI/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using ProjetoLivrariaAPI.Data.Intefaces;
+using ProjetoLivrariaAPI.Dtos.User;
+using ProjetoLivrariaAPI.Dtos.Validators;
+using ProjetoLivrariaAPI.Services.Interfaces;
+
+namespace ProjetoLivrariaAPI.Services {
+    public class UserService : IUserService {
+
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper) {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResultService<UserDto>> CreateAsync(UserDto userDto) {
+            if (userDto == null) {
+                return ResultService.Fail<UserDto>("Objeto deve ser informado !!");
+               
+                var result = new UserDtoValidator().Validate(userDto);
+                if (!result.IsValid)
+                    return ResultService.RequestError<UserDto>("Problemas de validação !", result);
+
+                var person = 
+            }
+        }
+    }
+}
