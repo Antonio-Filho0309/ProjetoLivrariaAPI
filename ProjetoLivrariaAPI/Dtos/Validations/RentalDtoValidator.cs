7a381f40ce4f99679bfb324703f4e697e964b964
Antonio Filho <antoniodollmichael@gmail.com>
2023-10-19 18:10:50 -0300

    hihi

--- ProjetoLivrariaAPI/Dtos/Validations/RentalDtoValidator.cs
+++ ProjetoLivrariaAPI/Dtos/Validations/RentalDtoValidator.cs
@@ -3,40 +3,38 @@ using ProjetoLivrariaAPI.Dtos.Rental;
 
 namespace ProjetoLivrariaAPI.Dtos.Validations {
     public class RentalDtoValidator : AbstractValidator<CreateRentalDto> {
-        public RentalDtoValidator()
-        {
+        public RentalDtoValidator() {
             RuleFor(r => r.BookId)
                .NotEmpty()
-               
+
                .WithMessage("Livro deve ser informado !");
 
             RuleFor(r => r.UserId)
                 .NotEmpty()
-                
+
                 .WithMessage("Usuário deve ser informado !");
 
             RuleFor(r => r.RentalDate)
                 .NotEmpty()
-                
+
                 .WithMessage("Data de aluguel deve ser informada !");
 
             RuleFor(r => r.PreviewDate)
                 .NotEmpty()
-                
+
                 .WithMessage("Data de previsão de aluguel deve ser informado");
         }
     }
 
     public class UpdateRentalDtoValidator : AbstractValidator<UpdateRentalDto> {
-        public UpdateRentalDtoValidator()
-        {
+        public UpdateRentalDtoValidator() {
             RuleFor(r => r.Id)
                 .NotEmpty()
-                
+
                 .WithMessage("Campo Id não informado.");
             RuleFor(x => x.ReturnDate)
                 .NotEmpty()
-                
+
                 .WithMessage("Campo Data de Devolução não informado.");
         }
     }
